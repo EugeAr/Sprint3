66f327458a884b9bd67e73f0518988f02300513c
EugeAr <134947049+EugeAr@users.noreply.github.com>
2023-10-26 13:06:40 -0300

    Update Bensimon.py

--- Bensimon.py
+++ Bensimon.py
@@ -35,7 +35,6 @@ class sprint3_user2(unittest.TestCase):
         driver = self.driver
         driver.get('https://bensimon.com.ar/customer/account/login/')
         time.sleep(3) 
-        #Cerrar el pop-up
         pop_up = driver.find_element(By.ID, registro.btn_popup_id)
         pop_up.click()
         time.sleep(2)
@@ -49,8 +48,6 @@ class sprint3_user2(unittest.TestCase):
         Lupa_buscador.click()
         titulo_pag = driver.title
         self.assertEqual(busqueda.txt_busqueda, titulo_pag, "No coincide con la búsqueda efectuada")
-        time.sleep(3)
-        llavero = driver.find_element(By.LINK_TEXT, busqueda.txt_busqueda_link_text).click()
         time.sleep(3)
     
     def test_1086(self):
@@ -79,20 +76,16 @@ class sprint3_user2(unittest.TestCase):
         boton_comprar.click()
         wait = WebDriverWait(driver, 10)
         mensaje_esperado = producto1_carrito.txt_compra
-        #Esperar a que el mensaje aparezca y obtener su texto
         leyenda_compra = wait.until(EC.presence_of_element_located((By.XPATH, producto1_carrito.txt_compra_xpath ))).text
-        # Realizar la aserción
         self.assertEqual(mensaje_esperado, leyenda_compra, "El mensaje de compra no coincide")
-   
         carrito= driver.find_element(By.XPATH, producto1_carrito.btn_carrito_xpath)
         carrito.click()
         time.sleep(3)
-
         titulo_producto = wait.until(EC.presence_of_element_located((By.XPATH, producto1_carrito.txt_producto_carrito_xpath ))).text
         self.assertEqual(busqueda.txt_busqueda_link_text, titulo_producto, "El producto en carrito no coincide")
-
         ver_detalles = driver.find_element(By.XPATH, producto1_carrito.dpd_detalles_xpath ).click()
         time.sleep(3)
+
     def tearDown(self):
         self.driver.close()
  
